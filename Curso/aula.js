2c8e6c6c7cd907eca296148e91101686ea252626
Ricardo Costa <rjdcosta@gmail.com>
2022-07-30 16:08:06 +0100

    init

---
+++ Curso/aula.js
@@ -0,0 +1,2 @@
+console.log('Ricardo teste')
+console.log('Benvindo ao Curso')
---
+++ Curso/scripts.js
@@ -0,0 +1,5 @@
+var array = [8,7,6,5,4,3,2,1];
+for (var i = 0, len = array.length; i < len; i++){
+    var element = array[i];
+    console.log(element);
+}
